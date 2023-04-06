5f72279061e60499032ec3fb0b3e19c1df5adfe8
kyleschneiderx <kyleschneiderx@gmail.com>
2023-04-06 06:47:11 -0600

    change

--- server/middleware/apikey.js
+++ server/middleware/apikey.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
+
 
 const api = (req,res, next) => {
     var token = req.headers["x-access-token"];
+    console.log(token)
     if(token !== process.env.RANDOMPROTECTION){
         return res.status(323).send("Wrong API Key")
     }else{
